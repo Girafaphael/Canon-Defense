62f219872a2da7710f15685abdc9612b5d26f28f
aia-e <100125186+aia-e@users.noreply.github.com>
2022-04-07 21:43:28 +0200

    Update DrawingView.kt

--- canondefense/DrawingView.kt
+++ canondefense/DrawingView.kt
@@ -8,14 +8,19 @@ import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import androidx.fragment.app.FragmentActivity
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class DrawingView @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0):
     SurfaceView(context, attributes,defStyleAttr), SurfaceHolder.Callback,Runnable {
 
+    var ElapsedTime = 0.toDouble()
+    val random = Random()
     var drawing = false
     var screenWidth = 0f
     var screenHeight = 0f
+    val TEMPSAPPARITION = 2.5
     private val backgroundPaint = Paint()
     private val canon = Canon(0f, 0f, 0f, 0f, this)
     val Ennemis = ArrayList<Ennemi>()
@@ -54,14 +59,24 @@ class DrawingView @JvmOverloads constructor (context: Context, attributes: Attri
             canvas.drawRect(0f, 0f, canvas.width.toFloat(),
                 canvas.height.toFloat(), backgroundPaint)
             canon.draw(canvas)
+            for (Ennemi in Ennemis)
+                Ennemi.draw(canvas)
             holder.unlockCanvasAndPost(canvas)
         }
     }
 
     override fun run() {
+        var previousFrameTime = System.currentTimeMillis()
         while (drawing) {
+            val currentTime = System.currentTimeMillis()
+            var elapsedTimeMS:Double=(currentTime-previousFrameTime).toDouble()
+            ElapsedTime += elapsedTimeMS / 1000.0
+            if (ElapsedTime > TEMPSAPPARITION){
+                Ennemis.add(Gobelin(this, random.nextFloat()*screenWidth, random.nextFloat()*screenHeight))
+                ElapsedTime-=TEMPSAPPARITION
+            }
             draw()
+            previousFrameTime = currentTime
         }
     }
-
 }
