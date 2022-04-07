9f1cede0e5443dbfbd4cb0819ff16e713d69dda7
aia-e <100125186+aia-e@users.noreply.github.com>
2022-04-07 21:46:58 +0200

    Update Gobelin.kt

--- canondefense/Gobelin.kt
+++ canondefense/Gobelin.kt
@@ -1,4 +1,16 @@
 package com.example.canondefense
 
-class Gobelin(): Ennemi() {
+import android.graphics.Canvas
+import android.graphics.Color
+
+
+class Gobelin(view: DrawingView, x1: Float, y1: Float): Ennemi(view, x1, y1) {
+    override val diametre = 30f
+    override var dx=2f
+    override var dy=2f
+
+    override fun draw(canvas: Canvas) {
+        ennemiPaint.color = Color.GREEN
+        super.draw(canvas)
+    }
 }
